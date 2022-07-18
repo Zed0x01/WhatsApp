a23cbcb047344b84dea6d64ff89ea6f57e321a53
Zed0x01 <sherifashrafmedia@gmail.com>
2022-07-18 11:34:54 +0200

    update Axios config

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const backend_URL = "https://whatsapp-clone-30.herokuapp.com/";
 const instance = axios.create({
-  baseURL: "http://localhost:9000/",
+  baseURL: backend_URL,
 });
 
 export default instance;
